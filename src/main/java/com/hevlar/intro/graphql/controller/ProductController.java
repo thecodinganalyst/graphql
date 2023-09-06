03b2e4f7b46912124f27820254a68b03ba883958
Dennis Cai <dennis@thecodinganalyst.com>
2023-09-06 10:58:25 +0800

    add controller based exception handling

--- src/main/java/com/hevlar/intro/graphql/controller/ProductController.java
+++ src/main/java/com/hevlar/intro/graphql/controller/ProductController.java
@@ -2,10 +2,16 @@ package com.hevlar.intro.graphql.controller;
 
 import com.hevlar.intro.graphql.model.Product;
 import com.hevlar.intro.graphql.service.ProductService;
+import graphql.GraphQLError;
+import graphql.schema.DataFetchingEnvironment;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -44,5 +50,16 @@ public class ProductController {
     @MutationMapping
     public Mono<Boolean> deleteProduct(@Argument String id){
         return productService.deleteProduct(id);
+    }
+
+    @GraphQlExceptionHandler
+    public GraphQLError handle(@NonNull Throwable ex, @NonNull DataFetchingEnvironment environment){
+        return GraphQLError
+                .newError()
+                .errorType(ErrorType.BAD_REQUEST)
+                .message(ex.getMessage())
+                .path(environment.getExecutionStepInfo().getPath())
+                .location(environment.getField().getSourceLocation())
+                .build();
     }
 }
